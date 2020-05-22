7f9ff929b629c5c9186aa38e049438fd8fa3f10e
ShahriarDhruvo <shahriarelahi3062@gmail.com>
2020-05-23 00:39:35 +0600

    React Template v1.0

---
+++ React-Project-Template/Template-v1.0/src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css"
+import "./styles/styles.scss"
+
+ReactDOM.render(<p>This is my react-template</p>, document.getElementById("app"));
